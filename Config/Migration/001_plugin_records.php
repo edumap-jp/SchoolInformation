b66a4409b67951bcc99270c7a28b8e32dd745e22
Ryuji AMANO <ryuji@ryus.co.jp>
2019-03-14 11:35:37 +0900

    Add: プラグインレコードマイグレーション

---
+++ Config/Migration/001_plugin_records.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Add plugin migration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+App::uses('Space', 'Rooms.Model');
+
+/**
+ * Add plugin migration
+ *
+ * @package NetCommons\PluginManager\Config\Migration
+ */
+class PluginRecords extends NetCommonsMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'plugin_records';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(),
+		'down' => array(),
+	);
+
+/**
+ * plugin data
+ *
+ * @var array $migration
+ */
+	public $records = array(
+		'Plugin' => array(
+			//日本語
+			array(
+				'language_id' => '2',
+				'is_origin' => true,
+				'is_translation' => true,
+				'key' => 'school_informations',
+				'namespace' => 'netcommons/school_informations',
+				'name' => '学校情報',
+				'type' => 1,
+				'default_action' => 'school_informations/view',
+				'default_setting_action' => 'school_information_frame_settings/edit',
+				'frame_add_action' => 'school_information_frame_settings/edit',
+				'display_topics' => 1,
+				'display_search' => 1,
+			),
+			//英語
+			array(
+				'language_id' => '1',
+				'is_origin' => false,
+				'is_translation' => true,
+				'key' => 'school_informations',
+				'namespace' => 'netcommons/school_informations',
+				'name' => 'School Information',
+				'type' => 1,
+				'default_action' => 'school_informations/view',
+				'default_setting_action' => 'school_information_frame_settings/edit',
+				'frame_add_action' => 'school_information_frame_settings/edit',
+				'display_topics' => 1,
+				'display_search' => 1,
+			),
+		),
+		'PluginsRole' => array(
+			array(
+				'role_key' => 'room_administrator',
+				'plugin_key' => 'school_informations'
+			),
+		),
+		//PluginsRoomは、beforeでセットする
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		$pluginName = $this->records['Plugin'][0]['key'];
+		$this->records['PluginsRoom'] = array(
+			//サイト全体
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::WHOLE_SITE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+			//パブリックスペース
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::PUBLIC_SPACE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+			//プライベートスペース
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::PRIVATE_SPACE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+			//グループスペース
+			array(
+				'room_id' => Space::getRoomIdRoot(Space::COMMUNITY_SPACE_ID, 'Room'),
+				'plugin_key' => $pluginName
+			),
+		);
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		$this->loadModels([
+			'Plugin' => 'PluginManager.Plugin',
+		]);
+
+		if ($direction === 'down') {
+			$this->Plugin->uninstallPlugin($this->records['Plugin'][0]['key']);
+			return true;
+		}
+
+		foreach ($this->records as $model => $records) {
+			if (!$this->updateRecords($model, $records)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
