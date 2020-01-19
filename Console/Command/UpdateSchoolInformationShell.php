ce20427c2b878e05e5acac7b5ad7d068386ff6c4
s-nakajima <nakajimashouhei@gmail.com>
2020-01-19 22:38:10 +0900

    add: 各学校サイトの学校情報を更新する処理追加 https://github.com/edumap-jp/Edumap2/issues/9

---
+++ Console/Command/UpdateSchoolInformationShell.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * 学校情報シェル
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+App::uses('AppShell', 'Console/Command');
+
+/**
+ * 学校情報シェル
+ *
+ * @property SchoolInformation $SchoolInformation
+ *
+ * @package Edumap\SchoolInformations\Console\Command
+ */
+class UpdateSchoolInformationShell extends AppShell {
+
+/**
+ * JSON フォーマット
+ *
+ * @var int
+ */
+	const JSON_FORMAT = JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+
+/**
+ * use model
+ *
+ * @var array
+ */
+	public $uses = [
+		'SchoolInformations.SchoolInformation',
+	];
+
+/**
+ * メイン処理
+ *
+ * @return void
+ */
+	public function main() {
+		try {
+			$this->SchoolInformation->begin();
+
+			$jsonContent = json_decode($this->param('json-content'), true);
+			if (! $this->__validateParams($jsonContent)) {
+				return false;
+			}
+
+			$edumapKey = $jsonContent['school_information']['edumap_key'];
+			$schoolInfo = $this->SchoolInformation->getSchoolInformation();
+
+			$schoolInfoAlias = $this->SchoolInformation->alias;
+			if (empty($schoolInfo[$schoolInfoAlias]) &&
+					$schoolInfo[$schoolInfoAlias]['edumap_key'] !== $edumapKey) {
+				$this->err('edumap_keyに対する学校情報がありません');
+				return false;
+			}
+
+			if ($schoolInfo[$schoolInfoAlias]['modified'] > $jsonContent['school_information']['modified']) {
+				//TODO: 公式サイトの更新JOBを実行する
+				return true;
+			}
+
+			$saveData = array_merge($schoolInfo[$schoolInfoAlias], $jsonContent['school_information']);
+
+			if (! $this->SchoolInformation->saveSchoolInformation([$schoolInfoAlias => $saveData])) {
+				$validationErrors = $this->SchoolInformation->validationErrors;
+				$this->err(json_encode($validationErrors, self::JSON_FORMAT));
+				return false;
+			}
+
+			//トランザクションCommit
+			$this->SchoolInformation->commit();
+		} catch (Exception $ex) {
+			//トランザクションRollback
+			$this->SchoolInformation->rollback($ex);
+		}
+
+		$this->out('<success>学校情報を更新しました</success>');
+		return true;
+	}
+
+/**
+ * 引数の入力チェック
+ *
+ * @param string|null $jsonContent JSONテキスト
+ * @return bool
+ */
+	private function __validateParams($jsonContent) {
+		if (! $jsonContent) {
+			$this->err('jsonテキストではありません');
+			return false;
+		}
+		if (empty($jsonContent['school_information'])) {
+			$this->err('フォーマットが異なります');
+			return false;
+		}
+		if (empty($jsonContent['school_information']['edumap_key'])) {
+			$this->err('edumap_keyが含まれていません');
+			return false;
+		}
+		if (empty($jsonContent['school_information']['modified'])) {
+			$this->err('modifiedが含まれていません');
+			return false;
+		}
+		return true;
+	}
+
+/**
+ * ヘルプオプションの取得
+ *
+ * @return ConsoleOptionParser
+ */
+	public function getOptionParser() {
+		$optionParser = parent::getOptionParser();
+		$optionParser->description('学校情報の更新')
+			->addOption(
+				'json-content',
+				[
+					'help' => 'JSONテキスト',
+					'required' => true,
+				]
+			);
+		return $optionParser;
+	}
+
+}
