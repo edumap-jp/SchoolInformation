787b26dcc3ccac73463d80ecc8ecaec754e87b73
s-nakajima <nakajimashouhei@gmail.com>
2021-06-04 14:42:35 +0900

    change: 許可IPにWillboosterを追加 https://github.com/edumap-jp/Edumap2/issues/118

--- Config/edumap_api.php
+++ Config/edumap_api.php
@@ -15,11 +15,13 @@ switch ($releaseType) {
 		//本番(edumap.jp)
 		$allowIps = '192.168.11.5|133.242.55.199'; //データ管理サーバ
 		$allowIps .= '|123.226.229.113'; //NaKaZii
+		$allowIps .= '|18.177.245.106'; //Willbooster
 		break;
 	case 'DEVELOP_SAKURA':
 		//開発さくらクラウド(dev.edumap.jp.jp)
 		$allowIps = '192.168.254.20'; //データ管理サーバ
 		$allowIps .= '|123.226.229.113'; //NaKaZii
+		$allowIps .= '|18.177.245.106'; //Willbooster
 		break;
 	default:
 		//ローカル開発
